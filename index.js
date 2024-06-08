d35f0ea217c7d8da74273bc165dd8c308d3b19ed
maycolS17 <138169411+maycolS17@users.noreply.github.com>
2024-06-08 01:56:35 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let menuVisible = false;
+//funcio que oculta y muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }
+    else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;   
+    }
+}
+
+function seleccionar(){
+    //funcion donde ocultamos el menu una ves que se seleccione una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
